e96340ae36f463589ff471c0285e09f55fc23f43
Adriano Netto <adriano.netto2010@gmail.com>
2025-11-28 00:23:41 -0300

    app finalizado

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 import Alpine from 'https://cdn.jsdelivr.net/npm/alpinejs@3.15.2/dist/module.esm.js'
-import * as docx from 'https://cdn.jsdelivr.net/npm/docx@9.5.1/+esm'
 import fileSaver from 'https://cdn.jsdelivr.net/npm/file-saver@2.0.5/+esm'
 import moment from 'https://cdn.jsdelivr.net/npm/moment@2.30.1/+esm'
-import { toDocx } from 'https://cdn.jsdelivr.net/npm/docshift@0.0.73/+esm'
 import Logger from './logger.js'
 import djen from './djen.js'
 import { filtrarPublicacoes } from  './utils.js'
@@ -58,60 +56,44 @@ document.addEventListener("alpine:init", () => {
             }
         },
 
-        filtrar() {
-            this.publicacoesFiltradas = this.publicacoes.filter(pub => {
-                return pub.destinatarios.some(dest => {
-
-                    const nome = dest.nome.toLowerCase()
-                        .normalize('NFD').replace(/[\u0300-\u036f]/g, '')
-                        .replace('-', ' ');
-
-                    const input = this.form.nomeParte.toLowerCase()
-                        .normalize('NFD').replace(/[\u0300-\u036f]/g, '')
-                        .replace('-', ' ');
-
-                    if(!nome.includes(input)) {
-                        return false;
-                    }
-
-                    return this.keywords.some(keyword => nome.includes(keyword));
-                })
-            })
-        },
-
         async salvar() {
-            const parser = new DOMParser();
-
-            let blocks = '';
+            let parts = [];
 
             this.publicacoesFiltradas.forEach(pub => {
-                blocks += `
-                    <p>
-                        <span class="publicacao-title">Processo ${pub.numeroprocessocommascara}</span><br>
-                        <span><strong>Órgão: </strong> ${pub.nomeOrgao}</span><br>
-                        <span><strong>Data de disponibilização: </strong>${moment(pub.data_disponibilizacao).format('DD/MM/YYYY')}</span><br>
-                        <span><strong>Tipo de comunicação: </strong>${pub.tipoComunicacao}</span><br>
-                        <span><strong>Meio: </strong> ${pub.meiocompleto}</span><br>
-                        ${pub.link ? `<span><strong>Inteiro teor: </strong></span>` : ''}
-                        ${pub.link ? `<a href="${pub.link}">Clique aqui</a><br>` : ''}
-                        <span><strong>Parte(s): </strong></span><br>
-                        ${pub.destinatarios.map( dest => `<span>${dest.nome}<span><br>`).join('')}
-                        ${pub.destinatarioadvogados.length ? `<span><strong>Advogado(s): </strong></span><br>` : ''}
-                        ${pub.destinatarioadvogados.length ? pub.destinatarioadvogados.map( dest => `<span>${dest.advogado.nome} - OAB ${dest.advogado.uf_oab}-${dest.advogado.numero_oab}<span><br>`).join('') : ''}
-                        <section">${pub.texto}</section><br>
-                        <br>
-                    </p>
-                `
+                parts.push(`<span>`);
+                parts.push(`<span class="publicacao-title">Processo ${pub.numeroprocessocommascara}</span><br>`);
+                parts.push(`<span><strong>Órgão: </strong> ${pub.nomeOrgao}</span><br>`);
+                parts.push(`<span><strong>Data de disponibilização: </strong>${moment(pub.data_disponibilizacao).format('DD/MM/YYYY')}</span><br>`);
+                parts.push(`<span><strong>Tipo de comunicação: </strong>${pub.tipoComunicacao}</span><br>`);
+                parts.push(`<span><strong>Meio: </strong> ${pub.meiocompleto}</span><br>`);
+                
+                if(pub.link) {
+                    parts.push(`<span><strong>Inteiro teor: </strong></span>`);
+                    parts.push(`<a href="${pub.link}">Clique aqui</a><br>`);
+                }
+                
+                parts.push(`<span><strong>Parte(s) </strong></span><br>`);
+                parts.push(pub.destinatarios.map(d => `<span>${d.nome}</span><br>`).join(''));
+                
+                if(pub.destinatarioadvogados.length) {
+                    parts.push(`<span><strong>Advogado(s) </strong></span><br>`);
+                    parts.push(pub.destinatarioadvogados.map(d => `<span>${d.advogado.nome} - OAB ${d.advogado.uf_oab}-${d.advogado.numero_oab}</span><br>`).join(''));
+                }
+                
+                parts.push(`<section>${pub.texto}</section><br>`);
+                parts.push(`</span>`);
+                parts.push(`<br>`);
             });
 
             const html = `<!DOCTYPE html>
-                <html lang="en">
+                <html lang="pt-BR">
                 <head>
                     <meta charset="UTF-8">
                     <style>
-                        .publicacao-title {
-                            color: #004BCB !important;
-                            font-weight: bold;
+                        body {
+                            font-family: 'Calibri Light'; 
+                            font-size: 13px; 
+                            color: #4C4C4C;
                         }
 
                         strong {
@@ -121,18 +103,21 @@ document.addEventListener("alpine:init", () => {
                         a {
                             text-decoration: none;
                         }
+
+                        .publicacao-title {
+                            color: #004BCB !important;
+                            font-weight: bold;
+                        }
                     </style>
                 </head>
-                <body style="font-family: 'Calibri Light'; font-size: 13px; color: #4C4C4C">
-                    ${ blocks }
+                <body>
+                    ${ parts.join('') }
                 </body>
                 </html>`;
 
             const converter = window.htmlDocx.asBlob;
             const docxBlob = await converter(html);
             fileSaver.saveAs(docxBlob, `publicações-${moment().format('DD-MM-YYYY')}.docx`);
-
-      
         },
     }));
 });
